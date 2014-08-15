d3ea946e8339e96c010db19eaca95661bb5a3936
knoblo <knoblo@rhrk.uni-kl.de>
2014-08-15 15:06:27 +0200

    Test verbessert

--- src/WohnungTest.java
+++ src/WohnungTest.java
@@ -6,35 +6,38 @@ public class WohnungTest extends TestCase {
 	private Swing_View swing;
 	private Wohnung wohnung;
 	private double höhe;
+	private String fläche;
 	
 	protected void setUp() {
 		swing = new Swing_View();
 		new Renovierung(swing);
 		wohnung = new Wohnung(swing);
-		höhe = 2.3;
+		höhe = 2.5;
+		fläche = Double.toString(Math.sqrt(30.6/4) * 2.5 * 4 * 4);
+		wohnung.setHöhe(höhe);
+		wohnung.setAnzahlRäume(4);
+		wohnung.setQuadratmeter(30.6);
+		wohnung.zu_streichende_fläche();
 	}
 	
 	protected void tearDown() {
 		swing = null;
 		wohnung = null;
 		höhe = 0.0;
+		fläche = null;
+		
 	}
 	
 
 	public void testsetHöhe() {
 		setUp();
-		wohnung.setHöhe(höhe);
 		assertEquals(höhe,wohnung.getHöhe());
 		tearDown();		
 	}
 	
 	public void testzu_streichende_fläche() {
 		setUp();
-		wohnung.setHöhe(2.5);
-		wohnung.setAnzahlRäume(4);
-		wohnung.setQuadratmeter(30.6);
-		wohnung.zu_streichende_fläche();
-		assertEquals(swing.getzustreichendeFläche(),Double.toString(Math.sqrt(30.6/4) * 2.5 * 4 * 4));
+		assertEquals(swing.getzustreichendeFläche(),fläche);
 		tearDown();
 		
 	}
